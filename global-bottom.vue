a1aaf8b75781a2a29527652ed374326be34d34b3
inody <i11235813@gmail.com>
2026-05-14 13:41:21 +0100

    Update footer to display total pages dynamically

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+
+const slidevContext = inject<Record<string, any> | undefined>('$$slidev-context', undefined)
+const frontmatter = inject<Record<string, string | number> | undefined>('$$slidev-fontmatter', undefined)
+
+const displayTotal = computed(() => slidevContext?.configs?.pageTotal ?? frontmatter?.pageTotal)
+</script>
+
 <template>
   <footer class="absolute bottom-0 left-226 right-0 p-2">
-    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+    {{ $slidev.nav.currentPage }} / {{ displayTotal ?? $slidev.nav.total }}
   </footer>
 </template>
